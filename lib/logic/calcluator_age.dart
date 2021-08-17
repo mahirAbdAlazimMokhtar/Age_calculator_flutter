9cbcc5daa69546f9d00f519745c8423ad5df5d84
mahir <mahr.bwb@gmail.com>
2021-08-17 06:24:05 +0200

    add ageCalcluator to get {age and next birth day}

---
+++ lib/logic/calcluator_age.dart
@@ -0,0 +1,14 @@
+import 'package:age_calculator/Models/age_model.dart';
+import 'package:age_calculator/Models/duration_model.dart';
+
+class AgeCalcluator {
+
+  Age calcluatorAge (DateTime birthday , DateTime today){
+    //this method to calculate your age
+
+  Duration calculatorNextBirthdayDuration (DateTime birthday , DateTime today){
+    //this method to calculate next birthday
+
+  }
+
+}

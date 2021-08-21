098cb1d78d6fc1ab8c48f9c91a437da54e7fdf3e
mahir <mahr.bwb@gmail.com>
2021-08-21 10:18:23 +0200

    add override to print custom string

--- lib/Models/age_model.dart
+++ lib/Models/age_model.dart
@@ -1,3 +1,8 @@
-class Age{
-  int years=0,months=0, day=0;
+class Age {
+  int years = 0, months = 0, days = 0;
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "age is :  years: $years - months : $months - days : $days";
+  }
 }
